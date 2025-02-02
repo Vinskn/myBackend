8707a9f45cac7464ab6d4dd9de72a0a2de7fc0bd
Vinskn <chuncumaru1@gmail.com>
2025-02-02 15:09:50 +0700

    Initial commit

---
+++ NAVAserver.mjs
@@ -0,0 +1,112 @@
+import 'dotenv/config';
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import cors from 'cors';
+import multer from 'multer';
+import path from 'path';
+import fs from 'fs';
+
+const app = express();
+
+// middleware
+app.use(cors({
+    origin: '*',
+    allowedHeaders: ['Content-Type']
+}));
+app.use(express.json());
+app.use('/upload', express.static('upload'));
+
+// storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const { collectName } = req.params;
+        let folder;
+        if (collectName === 'Project-Part') {
+            folder = 'project';
+        }
+        else if (collectName === 'Certificate-Part') {
+            folder = 'certif';
+        }
+
+        const folderPath = path.join(__dirname, 'upload', folder);
+        if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath, { recursive: true });
+        }
+        cb(null, folder);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const upload = multer({ storage })
+
+
+
+// connect to MongoDB
+const client = new MongoClient(process.env.uri);
+let db;
+const connection = async (dbName) => {
+    if (!db) {
+        try {
+            await client.connect();
+            console.log(`Connected to ${dbName} database.`);
+            db = client.db(dbName);
+        }
+        catch (e) {
+            console.error(`Error connecting to ${dbName} database:`, e);
+            process.exit(1);
+        }
+    }
+    return db;
+}
+
+
+// get endpoint
+app.get('/api/:dbName/:collectName', async(req, res) => {
+    const {dbName, collectName} = req.params;
+    try {   
+        const database = await connection(dbName);
+        const collectionName = await database.collection(collectName);
+        const result = await collectionName.find().toArray();
+        res.json(result);
+    }
+    catch (e) {
+        console.error(`Error retrieving data from ${dbName}.${collectName}:`, e);
+        res.status(500).json({ error: 'Failed to retrieve data.' });
+    }
+})
+
+
+// post endpoint
+app.post('/api/:dbName/:collectName', upload.single('image'), async (req, res) => {
+   const { dbName, collectName } = req.params;
+   try {
+        const database = await connection(dbName);
+        const collectionName = await database.collection(collectName);
+        let result;
+
+        if (req.file) {
+            const additionalData = req.body;
+            result = await collectionName.insertOne({
+                imagePath: `/upload/${collectName}/${req.file.filename}`,
+                additionalData
+            });
+        }
+        else {
+            const data = req.body;
+            result = await collectionName.insertOne(data);
+        }
+        res.json({ message: 'Data saved successfully.', insertedId: result.insertedId });
+   }
+   catch (e) {
+        console.error(`Error saving data to ${dbName}.${collectName}:`, e);
+        res.status(500).json({ error: 'Failed to save data.' });
+   }
+})
+
+
+// run server
+app.listen(5000, () => {
+    console.log("Server running...");
+});
+
